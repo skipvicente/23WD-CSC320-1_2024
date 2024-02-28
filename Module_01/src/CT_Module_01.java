8889a65ff7e3b73414550658ad39faeea16971d1
skipvicente <skipvicente2@aol.com>
2024-02-28 00:02:48 -0600

    Updates for 2-27-24 from home

--- Module_01/src/CT_Module_01.java
+++ Module_01/src/CT_Module_01.java
@@ -19,31 +19,36 @@ public class CT_Module_01 {
 		System.out.println("Please enter your first name");
 		Scanner fNameTemp = new Scanner(System.in ); // Entering First name here
 		fName= fNameTemp.nextLine(); //Updating User first name
-
+		fNameTemp.close(); 
 		
 		System.out.println("Please enter your last name");			
 		Scanner lNameTemp = new Scanner(System.in); // Entering Last name here
-		lName= lNameTemp.nextLine(); //Updating User last name		
+		lName= lNameTemp.nextLine(); //Updating User last name	
+		lNameTemp.close();	
 	
 		
 		System.out.println("Please enter your street NUMBER only");			
 		Scanner addressNumTemp = new Scanner(System.in); // Entering street address number
-		addressNum= addressNumTemp.nextInt(); //Updating User street number		
+		addressNum= addressNumTemp.nextInt(); //Updating User street number
+		addressNumTemp.close();		
 			
 
 		System.out.println("Please enter your street NAME only");			
 		Scanner addressNameTemp = new Scanner(System.in); // Entering street name
-		addressName= addressNameTemp.nextLine(); //Updating User street name			
+		addressName= addressNameTemp.nextLine(); //Updating User street name
+		addressNameTemp.close();				
 		
 
 		System.out.println("Please enter your city");			
 		Scanner aCityTemp = new Scanner(System.in); // Entering City
-		aCity= aCityTemp.nextLine(); //Updating User city	
+		aCity= aCityTemp.nextLine(); //Updating User city
+		aCityTemp.close();	
 		
 		
 		System.out.println("Please enter your street ZIP CODE");			
 		Scanner zipCodeTemp = new Scanner(System.in); // Entering street zip code
-		zipCode= zipCodeTemp.nextInt(); //Updating User zip code		
+		zipCode= zipCodeTemp.nextInt(); //Updating User zip code
+		zipCodeTemp.close();		
 		
 		
 		// System.out.println(fName + " " + lName + ": " + addressNum + " " + addressName + " " + zipCode); // Testing updated variable
